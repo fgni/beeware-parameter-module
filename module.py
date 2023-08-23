b5248131fbd0225a9ce82587bb4563931e561c26
fgni <fgn@dbmail.com>
2023-08-24 00:12:00 +0200

    Delete module.py

--- module.py
+++
@@ -1,9 +0,0 @@
-#def f(self, widget):
-#def f(self):
-def f(widget):
-    #print("module : ", self.f_var.value)
-    #print("module : ", self.f_var)
-    print("module : ", f_var)
-
-def g(self, widget, g_var):
-    print("module : ", g_var)
