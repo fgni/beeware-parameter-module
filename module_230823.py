e1e8404fd30ba10520f1f516aeaa03f924c40bed
fgni <fgn@dbmail.com>
2023-08-24 23:45:10 +0200

    Add files via upload

---
+++ module_230823.py
@@ -0,0 +1,11 @@
+def f(var1):
+    return "module.f : " + var1
+
+def g(var1):
+    return "module.g : " + var1
+
+def h(var1):
+    return "module.h : " + var1
+
+def k(var1):
+    return "module.k : " + var1
